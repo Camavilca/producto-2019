4a857a78fb8121aeb0e514c15f245a7a21a5da37
Camavilca <orlando.camavilca@tecsup.edu.pe>
2019-09-09 12:40:25 -0500

    fix

--- src/main/java/com/camavilca/dao/ProductoDAO.java
+++ src/main/java/com/camavilca/dao/ProductoDAO.java
@@ -1,7 +1,6 @@
 package com.camavilca.dao;
 
 import com.camavilca.model.Producto;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
